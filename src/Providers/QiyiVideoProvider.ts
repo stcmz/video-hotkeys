b3ffeb649f397ce06b32d44e7d295e4ce9273ab3
Maozi Chen <mc@imozo.us>
2021-09-19 08:29:36 -0400

    Enable play/pause with new key K

--- src/Providers/QiyiVideoProvider.ts
+++ src/Providers/QiyiVideoProvider.ts
@@ -52,7 +52,7 @@ export class QiyiVideoProvider extends VideoProvider {
     }
 
     commands: VideoCommands = {
-        play: this.nullCommand(),
+        play: this.playCommand(),
 
         speed: (up: boolean): Command => this.speedCommand(up),
 
