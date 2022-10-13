129daf80a5fad578994564cccec47d1f63718e15
Maozi Chen <mc@imozo.us>
2022-10-13 02:19:27 -0400

    Add playback rates 3x and 4x

--- src/Settings.ts
+++ src/Settings.ts
@@ -4,6 +4,6 @@ export class Settings {
     static verboseLog: boolean = false;
     static defaultOverlayDuration: number = 500;
     static unmuteOnVolumeChange: boolean = true;
-    static speedBrackets: number[] = [0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0];
+    static speedBrackets: number[] = [0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 3.0, 4.0];
     static autoHideDanmu: boolean = true;
 }
